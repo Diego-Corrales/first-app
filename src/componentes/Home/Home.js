d151f52e0f45d70785751440d47ab66477ca73a5
unknown <docarquitectura@hotmail.com>
2023-04-04 15:37:55 -0300

    agregue home

--- src/componentes/Home/Home.js
+++ src/componentes/Home/Home.js
@@ -2,21 +2,121 @@
 
 export const Home = () => {
     return (
-        <main className="container w-full max-h-screen">
+        <main className="w-full h-full">
             <div className="text-center">
-                <h2 className="text-3xl p-4">Bienvenidos a nuestra disqueria</h2>
-                <p className="text-2xl p-4">En nuestra pagina encontraran la coleccion de discos mas variada y especial, asi como tambien el mejor Merchandising "oficial" de tus bandas favoritas</p>
+                <h2 className="text-6xl py-8">BIENVENIDOS</h2>
+                <p className="text-3xl py-4">Todos los discos de tus bandas favoritas las encontraras solo en "Viking Records"</p>
             </div>
-            <section className="flex flex-wrap">
+            <section className="flex flex-wrap py-4">
                 <div className="w-full md:w-2/3 p-6">
-                    <h4>Carousel</h4>
+                    
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                        <div className="grid gap-4">
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="http://pm1.narvii.com/6370/272b2083fadecaf853f102b58667d4c0bb7653ee_00.jpg" alt=""></img>
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://rock101online.mx/wp-content/uploads/2022/05/f850x638-27651_105140_5050.jpg" alt=""></img>
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/originals/8d/81/3d/8d813d0b5c42267693d21fe7a36fa96b.jpg" alt=""></img>
+                            </div>
+                        </div>
+                        <div className="grid gap-4">
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/736x/7d/c7/19/7dc719bb54eb97d1173edd6a2e9c4915.jpg" alt=""></img>
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://www.runneruprecords.com/wp-content/uploads/2020/06/tercer-disco-de-avenged-sevenfold-city-of-evil.png" alt=""></img>
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="http://cdn.shopify.com/s/files/1/0223/4339/products/image_2155509_d339ed46-6b76-417d-998b-959c5e9577de_grande.jpg?v=1631201644" alt=""></img>
+                            </div>
+                        </div>
+                        <div className="grid gap-4">
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/474x/b5/ea/24/b5ea24066b4fc1bbf9e4598ff5284fbb.jpg" alt=""></img>
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://w0.peakpx.com/wallpaper/878/221/HD-wallpaper-ghost-banda-ghost-ghost-band-papa-emeritus-thumbnail.jpg" alt=""></img>
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://pm1.narvii.com/6488/8c05bd11c52dc9482f305c7ac9378c88e789968d_hq.jpg" alt=""></img>
+                            </div>
+                        </div>
+                        <div className="grid gap-4">
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/564x/53/fd/64/53fd640f871281a1629e2422f8f8d809.jpg" alt=""></img>
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://i.pinimg.com/564x/d9/3e/e5/d93ee5620a990a3ddf14e1097d5c8a5f--greatest-hits-scorpion.jpg" alt=""></img>
+                            </div>
+                            <div>
+                                <img className="h-auto max-w-full rounded-lg" src="https://p4.wallpaperbetter.com/wallpaper/458/754/371/babes-band-brunettes-females-wallpaper-preview.jpg" alt=""></img>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                <div className="w-full md:w-1/3 p-6">
-                    <h4>Eslogan</h4>
+                <div className="w-full md:w-1/3 p-6 mx-auto text-center text-4xl font-semibold flex flex-col justify-around">
+                    <p>ROCK</p>
+                    <p>HEAVY METAL</p>
+                    <p>BLACK METAL</p>
+                    <p>FOLK METAL</p>
+                    <p>INDUSTRIAL METAL</p>
+                    <p>Y MUCHOS MAS</p>
                 </div>
             </section>
-            <div className="text-center">
-                <h3 className="p-6">NOTICIAS Y SHOWS</h3>
+            <hr />
+            <div className="text-center py-10">
+                <h3 className="p-6 text-6xl">RECITALES QUE NO TE PODES PERDER</h3>
+                <section className="flex flex-wrap">
+                    <div className="w-full md:w-1/3 p-6">
+                        
+                        <a href="https://www.bandsintown.com/f/115865-masters-of-rock-2023?came_from=209" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://es.concerts-metal.com/images/flyers/202302/1675418358.jpg" alt=""></img>
+                            <div className="flex flex-col justify-between p-14 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">MASTERS OF ROCK <br /> 28/04/2023</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Adquiri tus entradas ingresando aqui</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div className="w-full md:w-1/3 p-6">
+                        
+                        <a href="https://www.allaccess.com.ar/event/vorterix-metal-fest" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="http://stargateproductions.net/wp-content/uploads/2022/12/dos.jpeg" alt=""></img>
+                            <div className="flex flex-col justify-between p-14 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">VORTERIX METAL <br /> 25/04/2023</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Adquiri tus entradas ingresando aqui</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div className="w-full md:w-1/3 p-6">
+                        
+                        <a href="https://www.passline.com/eventos/stratovarius-en-argentina" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://rocktambulos.com/wp-content/uploads/Sonata-Arctica-Arg-2023-story-576x1024.jpeg" alt=""></img>
+                            <div className="flex flex-col justify-between p-14 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">STRATOVARIUS <br /> 26/04/2023</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Adquiri tus entradas ingresando aqui</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div className="w-full md:w-1/3 p-6">
+                        
+                        <a href="https://www.passline.com/eventos/stratovarius-en-argentina" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://cuarteldelmetal.com/wp-content/uploads/2023/04/338697309_627016609267522_8951536638687469342_n-457x720.jpg" alt=""></img>
+                            <div className="flex flex-col justify-between p-14 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">GHOST <br /> 24/09/2023</h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Adquiri tus entradas ingresando aqui</p>
+                            </div>
+                        </a>
+
+                    </div>
+                    
+                </section>
 
             </div>
 
